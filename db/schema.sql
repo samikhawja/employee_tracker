af1dc2517c3de181bfe7ddaee19a1dbf59346122
samikhawja <skhawja11@gmail.com>
2021-10-24 15:44:47 -0700

    fixed schemaSQL

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT AUTO_INCREMENT,
+    department_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
     ON DELETE SET NULL
