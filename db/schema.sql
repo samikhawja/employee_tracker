efd5ee96d2b263aebdbe06c3e976465cbd88d367
samikhawja <skhawja11@gmail.com>
2021-10-25 09:56:23 -0700

    fixed schemaSQL to run seedsSQL without errors in terminal

--- db/schema.sql
+++ db/schema.sql
@@ -29,7 +29,5 @@ CREATE TABLE employees (
     manager_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
     ON DELETE SET NULL
 );
