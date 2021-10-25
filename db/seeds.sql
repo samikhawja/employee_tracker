b8f9bc2b5b479a8846d713b92c70b41b455b28a9
samikhawja <skhawja11@gmail.com>
2021-10-25 16:20:39 -0700

    added questions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,20 @@
-INSERT INTO department (id, name) VALUES
-    (1, "Sales"),
-    (2, "Engineer"),
-    (3, "C-Level");
+INSERT INTO department (name) VALUES
+    ("Sales"),
+    ("Engineer"),
+    ("C-Level");
 
-INSERT INTO role (id, title, salary, department_id) VALUES
-    (1, "Retailer", 45000, 1),
-    (2, "Sales Lead", 55000, 1),
-    (3, "Junior Engineer", 125000, 2),
-    (4, "Senior Engineer", 300000, 2),
-    (5, "COO", 750000, 3),
-    (6, "CEO", 1000000, 3);
+INSERT INTO role (title, salary, department_id) VALUES
+    ("Retailer", 45000, 1),
+    ("Sales Lead", 55000, 1),
+    ("Junior Engineer", 125000, 2),
+    ("Senior Engineer", 300000, 2),
+    ("COO", 750000, 3),
+    ("CEO", 1000000, 3);
 
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id) VALUES
-    (1, "Andres", "Lui", 1, 2),
-    (2, "Daniel", "Vo", 2, null),
-    (3, "Matt", "Weichel", 3, 4),
-    (4, "Sami", "Khawja", 4, null),
-    (5, "Manuel", "Nunes", 5, 6),
-    (6, "Jerome", "Chenette", 6, null);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
+    ("Daniel", "Vo", 2, null),
+    ("Andres", "Lui", 1, 2),
+    ("Sami", "Khawja", 4, null),
+    ("Matt", "Weichel", 3, 4),
+    ("Jerome", "Chenette", 6, null);
+    ("Manuel", "Nunes", 5, 6),
--- server.js
+++ server.js
@@ -1,21 +1,88 @@
 const inquirer = require('inquirer');
-const express = require('express');
-const mysql = require('mysql2');
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const express = require('express');
+// const mysql = require('mysql2');
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
-const db = mysql.createConnection(
+// const db = mysql.createConnection(
+//     {
+//         host: "localhost",
+//         user: "root",
+//         password: "password",
+//         database: "tracker_db"
+//     }
+// )
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+const questions = [
     {
-        host: "localhost",
-        user: "root",
-        password: "password",
-        database: "tracker_db"
-    }
-)
+        type: "list",
+        message: "Main Menu",
+        name: "mainMenu",
+        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"],
+    },
+    {
+        type: "input",
+        message: "What is the name of the department?",
+        name: "addDepartment",
+        when: () => answer.mainMenu === "add a department"
+    },
+    {
+        type: "input",
+        message: "",
+        name: "roleName",
+        when: () => answer.mainMenu === "add a role"
+    },
+    {
+        type: "input",
+        message: "",
+        name: "roleSalary",
+        when: () => answer.mainMenu === "add a role"
+    },
+    {
+        type: "list",
+        message: "",
+        name: "roleDepartment",
+        choices: [],
+        when: () => answer.mainMenu === "add a role"
+    },
+    {
+        type: "input",
+        message: "",
+        name: "employeeFirst",
+        when: () => answer.mainMenu === "add an employee"
+    },
+    {
+        type: "input",
+        message: "",
+        name: "employeeLast",
+        when: () => answer.mainMenu === "add an employee"
+    },
+    {
+        type: "input",
+        message: "",
+        name: "employeeRole",
+        when: () => answer.mainMenu === "add an employee"
+    },
+    {
+        type: "input",
+        message: "",
+        name: "employeeManager",
+        when: () => answer.mainMenu === "add an employee"
+    },
+];
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+function init(){
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+        console.log(response);
+    });
+}
+init();
 
-app.listen(PORT, () => {
-    console.log(`listening on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`listening on http://localhost:${PORT}`);
+// });
