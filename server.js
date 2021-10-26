6ce18d19d8b99f9d9708ef11fff28e92fe88d4aa
samikhawja <skhawja11@gmail.com>
2021-10-25 17:43:48 -0700

    restructed questions and tested

--- server.js
+++ server.js
@@ -1,88 +1,116 @@
 const inquirer = require('inquirer');
-// const express = require('express');
-// const mysql = require('mysql2');
-// const app = express();
-// const PORT = process.env.PORT || 3001;
+const mysql = require('mysql2');
+const table = require('console.table');
 
-// const db = mysql.createConnection(
-//     {
-//         host: "localhost",
-//         user: "root",
-//         password: "password",
-//         database: "tracker_db"
-//     }
-// )
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-const questions = [
+const db = mysql.createConnection(
     {
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "tracker_db"
+});
+
+db.connect((err) => {
+    if(err) throw err;
+    console.log("\n Plese select one of the following below \n");
+    menu();
+});
+
+function menu(){
+    inquirer.prompt({
         type: "list",
         message: "Main Menu",
-        name: "mainMenu",
+        name: "menu",
         choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"],
-    },
-    {
-        type: "input",
-        message: "What is the name of the department?",
-        name: "addDepartment",
-        when: () => answer.mainMenu === "add a department"
-    },
-    {
-        type: "input",
-        message: "",
-        name: "roleName",
-        when: () => answer.mainMenu === "add a role"
-    },
-    {
-        type: "input",
-        message: "",
-        name: "roleSalary",
-        when: () => answer.mainMenu === "add a role"
-    },
-    {
-        type: "list",
-        message: "",
-        name: "roleDepartment",
-        choices: [],
-        when: () => answer.mainMenu === "add a role"
-    },
-    {
-        type: "input",
-        message: "",
-        name: "employeeFirst",
-        when: () => answer.mainMenu === "add an employee"
-    },
-    {
-        type: "input",
-        message: "",
-        name: "employeeLast",
-        when: () => answer.mainMenu === "add an employee"
-    },
-    {
-        type: "input",
-        message: "",
-        name: "employeeRole",
-        when: () => answer.mainMenu === "add an employee"
-    },
-    {
-        type: "input",
-        message: "",
-        name: "employeeManager",
-        when: () => answer.mainMenu === "add an employee"
-    },
-];
-
-function init(){
-    inquirer
-    .prompt(questions)
-    .then((response) => {
-        console.log(response);
+    }).then((select) => {
+        switch(select.menu){
+            case "view all departments": viewDepartment(); break;
+            case "view all roles": viewRoles(); break;
+            case "view all employees": addDepartment(); break;
+            case "add a department": addDepartment(); break;
+            case "add a role": addDepartment(); break;
+            case "add an employee": addDepartment(); break;
+            case "update an employee role": addDepartment(); break;
+        }
     });
 }
-init();
 
-// app.listen(PORT, () => {
-//     console.log(`listening on http://localhost:${PORT}`);
-// });
+function viewDepartment() {
+    const query = "SELECT * FROM department;";
+    db.query(query, function (err, res) {
+        console.table(res);
+        menu();
+    })
+}
+
+function viewRoles() {
+    const query = "SELECT * FROM role;";
+    db.query(query, function (err, res) {
+        console.table(res);
+        menu();
+    })
+}
+
+
+
+
+// {
+//     type: "input",
+//     message: "What is the name of the department?",
+//     name: "addDepartment",
+//     when: () => answer.menu === "add a department"
+// },
+// {
+//     type: "input",
+//     message: "",
+//     name: "roleName",
+//     when: () => answer.menu === "add a role"
+// },
+// {
+//     type: "input",
+//     message: "",
+//     name: "roleSalary",
+//     when: () => answer.menu === "add a role"
+// },
+// {
+//     type: "list",
+//     message: "",
+//     name: "roleDepartment",
+//     choices: [],
+//     when: () => answer.menu === "add a role"
+// },
+// {
+//     type: "input",
+//     message: "",
+//     name: "employeeFirst",
+//     when: () => answer.menu === "add an employee"
+// },
+// {
+//     type: "input",
+//     message: "",
+//     name: "employeeLast",
+//     when: () => answer.menu === "add an employee"
+// },
+// {
+//     type: "input",
+//     message: "",
+//     name: "employeeRole",
+//     when: () => answer.menu === "add an employee"
+// },
+// {
+//     type: "input",
+//     message: "",
+//     name: "employeeManager",
+//     when: () => answer.menu === "add an employee"
+// },
+
+// function init(){
+//     inquirer
+//     .prompt(questions)
+//     .then((response) => {
+//         if(response.menu === "done"){
+//             console.log("Session Complete")
+//         }
+//     });
+// }
+// init();
